f2fa21aa8de458e4045a7b6e912be367a3550fae
Venkatesh Kavuluri <vkavuluri@paypalcorp.com>
2015-09-05 17:00:22 -0700

    Pusher Play

---
+++ server/say copy.py
@@ -0,0 +1,10 @@
+import pusher
+
+p = pusher.Pusher(
+  app_id='140107',
+  key='3f344b89f59af5cd7cc1',
+  secret='41325f033451e0187b4b',
+  ssl=True,
+  port=443
+)
+p.trigger('test_channel', 'my_event', {'message': 'hello sai!'})
